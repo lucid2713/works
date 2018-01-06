a34c5b69e1e3f2c38d0ab89216a0d59367c89108
Mina Kim <lucid2713@gmail.com>
2018-01-06 23:11:31 +0100

    p5.js added

---
+++ sketch.js
@@ -0,0 +1,34 @@
+function setup() {
+
+
+  var canvas = createCanvas(displayWidth, displayHeight);
+
+  // Move the canvas so it's inside our <div id="sketch-holder">.
+  canvas.parent('sketch-holder');
+  // createCanvas(windowWidth, windowHeight);
+  background(255);
+    colorEllipse = color(0, 51, 255);
+    colorRect = color(255, 0, 208);
+
+     frameRate(30);
+}
+
+function draw() {
+  noStroke();
+  // rectMode(CENTER);
+  fill(255, 10);
+  rect(0, 0, displayWidth, displayHeight);
+  // rect(0, 0, 720, 500);
+
+    if(mouseX != 0 && mouseY !=0){
+      noFill();
+      strokeWeight(0.3);
+      stroke(colorRect);
+      rect(mouseX-100, mouseY-100, 200, 200);
+      // rect(mouseX-50, mouseY-50, 100, 100);
+    }
+}
+
+function windowResized() {
+  centerCanvas();
+}
